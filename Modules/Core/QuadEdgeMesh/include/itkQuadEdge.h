ab85dd51ee17b9028d6e51b7475d16d759413eb0
Zach Williamson <zachary-williamson@uiowa.edu>
2015-05-27 11:35:50 -0500

    WIP: Make the Core/QuadEdgeMesh a shared library
    Change-Id: Ibd1563edb085e97e9305784972b1c2e7926d07fe

--- Modules/Core/QuadEdgeMesh/include/itkQuadEdge.h
+++ Modules/Core/QuadEdgeMesh/include/itkQuadEdge.h
@@ -19,6 +19,7 @@
 #define itkQuadEdge_h
 
 #include "itkQuadEdgeMeshBaseIterator.h"
+#include "itkQuadEdgeMeshExport.h"
 
 #include "itkMacro.h"
 
@@ -220,7 +221,7 @@ namespace itk
  * \ingroup ITKQuadEdgeMesh
  */
 
-class QuadEdge
+class ITKQuadEdgeMesh_EXPORT QuadEdge
 {
 public:
   /** Hierarchy typedefs & values. */
